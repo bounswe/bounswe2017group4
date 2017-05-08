305f70f427904ac63143e1566b67ee5fe04be4d1
Tuna Kağan Yılmaz <tuna.yilmaz@boun.edu.tr>
2017-05-08 21:58:34 +0300

    add test for get_comments

--- api/test_api.py
+++ api/test_api.py
@@ -4,7 +4,7 @@ from app import get_books, get_comments
 
 class TestApp(unittest.TestCase):	
 	def test_get_books(self):
-		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/books']).replace("\r\n", "\n").partition("{")[2]		
+		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/books']).partition("{")[2]		
 		self.assertEqual(output,"""
   "books": [
     {
@@ -20,6 +20,32 @@ class TestApp(unittest.TestCase):
       "price": 8.5
     }
   ]
+}
+""")
+
+	def test_get_comments(self):
+		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/comments']).partition("{")[2]		
+		self.assertEqual(output,"""
+	"comments" : [
+    {
+        "id": 1,
+        "book": 1,
+        "owner": 1,
+        "content": u"Such an enlightening book."
+    },
+    {
+        "id": 2,
+        "book": 1,
+        "owner": 2,
+        "content": u"Great book. Similar to 1984, only more realistic."
+    },
+    {
+        "id": 3,
+        "book": 2,
+        "owner":1,
+        "content": u"Very interesting."
+    }
+	]
 }
 """)
 if __name__=='__main__':
