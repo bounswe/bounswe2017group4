2337d05da0a696940032f294340dcfb359d77356
gurbuzbeyza <beygrbz@gmail.com>
2017-05-08 23:59:08 +0300

    added test_get_templates method [issue#74]

--- api/test_api.py
+++ api/test_api.py
@@ -1,10 +1,10 @@
 #!flask/bin/python
 import os, sys, subprocess, unittest
-from app import get_books, get_comments
+from app import get_books, get_comments, get_templates
 
 class TestApp(unittest.TestCase):	
 	def test_get_books(self):
-		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/books']).replace("\r\n", "\n").partition("{")[2]		
+		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/books']).partition("{")[2]		
 		self.assertEqual(output,"""
   "books": [
     {
@@ -22,29 +22,77 @@ class TestApp(unittest.TestCase):
   ]
 }
 """)
+
 	def test_get_comments(self):
-		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/comments']).replace("\r\n", "\n").partition("{")[2]		
+		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/comments']).partition("{")[2]		
 		self.assertEqual(output,"""
-	"comments" : [
+  "comments": [
     {
-        "id": 1,
-        "book": 1,
-        "owner": 1,
-        "content": u"Such an enlightening book."
-    },
+      "book": 1, 
+      "content": "Such an enlightening book.", 
+      "id": 1, 
+      "owner": 1
+    }, 
     {
-        "id": 2,
-        "book": 1,
-        "owner": 2,
-        "content": u"Great book. Similar to 1984, only more realistic."
-    },
+      "book": 1, 
+      "content": "Great book. Similar to 1984, only more realistic.", 
+      "id": 2, 
+      "owner": 2
+    }, 
     {
-        "id": 3,
-        "book": 2,
-        "owner":1,
-        "content": u"Very interesting."
+      "book": 2, 
+      "content": "Very interesting.", 
+      "id": 3, 
+      "owner": 1
     }
-]
+  ]
+}
+""")
+	def test_get_users(self):
+		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/users']).partition("{")[2]		
+		self.assertEqual(output,"""
+  "users": [
+    {
+      "id": 1, 
+      "name": "nicholasramsey"
+    }, 
+    {
+      "id": 2, 
+      "name": "richardterry"
+    }
+  ]
+}
+""")
+  
+	def test_get_templates(self):
+		output = subprocess.check_output(['curl','-u','group4:1111','-i','http://localhost:5000/api/templates']).partition("{")[2]    
+		self.assertEqual(output,"""
+  "templates": [
+    {
+      "id": 1, 
+      "in": [
+        "My name is $UserName", 
+        "I am $UserName"
+      ], 
+      "name": "Asking name", 
+      "out": [
+        "Hello $UserName.", 
+        "Nice to meet you $UserName!"
+      ]
+    }, 
+    {
+      "id": 2, 
+      "in": [
+        "I want to search books about $Genre", 
+        "Can you show me the books about $Genre"
+      ], 
+      "name": "Asking book", 
+      "out": [
+        "Here are some books about $Genre. Do you want to keep searching?"
+      ]
+    }
+  ]
+}
 """)
 if __name__=='__main__':
 	unittest.main(exit=False)
