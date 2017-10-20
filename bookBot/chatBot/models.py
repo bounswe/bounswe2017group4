e95730a105bc195f6afb5d8f173506b61c3a1d51
Tuna Kağan Yılmaz <tuna.yilmaz@boun.edu.tr>
2017-10-21 00:22:52 +0300

    3 tables added
    UserComment
    Nodes
    History
    Not migrated

--- bookBot/chatBot/models.py
+++ bookBot/chatBot/models.py
@@ -24,3 +24,18 @@ class UserRating(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	rating = models.IntegerField()
 	book_id = models.CharField(max_length=100)
+
+class UserComment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	comment = models.CharField(max_length=500)
+    book_id = models.IntegerField()
+
+class Nodes(models.Model):
+    node_id = models.IntegerField(unique=True)
+    parent_id = models.IntegerField()
+    content = models.CharField(max_length=200)
+
+class History(models.Model):
+    hist_id = models.IntegerField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    query = models.CharField(max_length=500)
