0bfb5cdecc53c07c088725d2504398b552e44fce
emreun <emreun92@gmail.com>
2017-10-20 12:46:02 +0300

    User, UserRating, UserComment models implemented.

--- bookBot/chatBot/models.py
+++ bookBot/chatBot/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	name = models.CharField(max_length=200)
+	password = models.CharField(max_length=100)
+	created_at = models.DateTimeField('date published')
+
+class UserInterest(models.Model):
+	INTEREST_TYPE_CHOICES = (
+		('author', 'Author'),
+		('category', 'Category'),
+	)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	year_in_school = models.CharField(
+		max_length=10,
+		choices=INTEREST_TYPE_CHOICES,
+		default='category',
+	)
+	interest = models.CharField(max_length=200)
+
+class UserRating(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	comment = models.CharField(max_length=255)
+	book_id = models.CharField(max_length=100)
