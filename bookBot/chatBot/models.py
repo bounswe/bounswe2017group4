1acb7d23c11a4c4bc5ba0aa0f9bdc9699e89bf3d
Tuna Kağan Yılmaz <tuna.yilmaz@boun.edu.tr>
2017-11-11 15:53:06 +0300

    New database tables added
    Edge table
    Response table

--- bookBot/chatBot/models.py
+++ bookBot/chatBot/models.py
@@ -3,39 +3,46 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-	name = models.CharField(max_length=200)
-	password = models.CharField(max_length=100)
-	created_at = models.DateTimeField('date published')
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField('date published')
+    telegram_id = models.IntegerField(unique=True, null=True)
 
 class UserInterest(models.Model):
-	INTEREST_TYPE_CHOICES = (
-		('author', 'Author'),
-		('category', 'Category'),
-	)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	interest_type = models.CharField(
-		max_length=10,
-		choices=INTEREST_TYPE_CHOICES,
-		default='category',
-	)
-	interest = models.CharField(max_length=200)
+    INTEREST_TYPE_CHOICES = (
+        ('author', 'Author'),
+        ('category', 'Category'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    interest_type = models.CharField(
+        max_length=10,
+        choices=INTEREST_TYPE_CHOICES,
+        default='category',
+    )
+    interest = models.CharField(max_length=200)
 
 class UserRating(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	rating = models.IntegerField()
-	book_id = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    book_id = models.CharField(max_length=100)
 
 class UserComment(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	comment = models.CharField(max_length=500)
-	book_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=500)
+    book_id = models.IntegerField()
 
-class Nodes(models.Model):
-	node_id = models.IntegerField(unique=True)
-	parent_id = models.IntegerField()
-	content = models.CharField(max_length=200)
+class Edge(models.Model):
+    id = models.IntegerField(primary_key=True)
+    node_id = models.IntegerField()
+    user_response = models.CharField(max_length=200)
+    next_node_id = models.IntegerField()
+
+class Response(models.Model):
+    id = models.IntegerField(primary_key=True)
+    edge_id = models.ForeignKey(Edge)
+    chatbot_response = models.CharField(max_length=500)
 
 class History(models.Model):
-	hist_id = models.IntegerField(unique=True)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	query = models.CharField(max_length=500)
+    hist_id = models.IntegerField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    query = models.CharField(max_length=500)
