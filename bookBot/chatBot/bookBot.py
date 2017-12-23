2f1bbb5e5fdf0b072e63d27fabeb33eda551a0ab
Tuna Kağan Yılmaz <tuna.yilmaz@boun.edu.tr>
2017-12-23 17:15:51 +0300

    new field added to Edge model and one POST method is added

--- bookBot/chatBot/bookBot.py
+++ bookBot/chatBot/bookBot.py
@@ -21,7 +21,7 @@ if(os.environ.get('RUNMODE')=="test"):
 	updater = Updater(token='471766784:AAHJPT82C21DvW_EhZXZ9fEQdS9a94mIYs0')
 else :
 	print("Running in prod mode")
-	updater = Updater(token='471766784:AAHJPT82C21DvW_EhZXZ9fEQdS9a94mIYs0')
+	updater = Updater(token='306155790:AAHshYWFsAmOKly8107HkSISlUziQz77DLs')
 
 dispatcher = updater.dispatcher
 client = Wit(access_token=access_token)
