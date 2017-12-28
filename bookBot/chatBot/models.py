060a1cac2fde8e7d7a27ee31a1ea27b15a111324
emreun <emreun92@gmail.com>
2017-12-28 16:37:23 +0200

    Autofields converted into IntegerField

--- bookBot/chatBot/models.py
+++ bookBot/chatBot/models.py
@@ -42,7 +42,7 @@ class UserRating(models.Model):
 
 
 class UserComment(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.CharField(max_length=500, blank=True)
     book_id = models.CharField(max_length=100)
@@ -52,7 +52,7 @@ class UserComment(models.Model):
 
 
 class State(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     description=models.CharField(max_length=500)
 
     def __str__(self):
